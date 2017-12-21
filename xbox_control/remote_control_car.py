205304a84f64579e935ba19b6af775bd636ba431
Kyle Higginson <kylehigginson11@hotmail.com>
2017-12-21 01:24:35 +0000

    Adding Xbox Controller support to car

---
+++ xbox_control/remote_control_car.py
@@ -0,0 +1,49 @@
+import xbox
+from car_control.car import Car
+
+
+# Format floating point number to string format -x.xxx
+def convert_float(n):
+    return '{:6.3f}'.format(n)
+
+
+joy = xbox.Joystick()
+car = Car(9, 6)
+
+print ("Start controlling car remotely, press back to quit ...")
+# Loop until back button is pressed
+while not joy.Back():
+    # Connection status
+    if joy.connected():
+        print ("Connected   "),
+    else:
+        print ("Disconnected"),
+    # Left analog stick
+    print ("Left Stick X: {}, Left Stick Y: {}".format(convert_float(joy.leftX()), convert_float(joy.leftY()))),
+    # Right trigger
+    print ("Right Trigger: ", convert_float(joy.rightTrigger())),
+    # A, B, X, Y buttons
+    if joy.A():
+        car.reverse()
+    elif joy.B():
+        car.right()
+    elif joy.X():
+        car.left()
+    elif joy.Y():
+        car.forward()
+    # DPAD, up, down, left or right
+    elif joy.dpadUp():
+        car.forward()
+    elif joy.dpadDown():
+        car.reverse()
+    elif joy.dpadLeft():
+        car.left()
+    elif joy.dpadRight():
+        car.right()
+    else:
+        car.stop()
+
+    # Move cursor back to start of line
+    print chr(13),
+# Close out when done
+joy.close()
