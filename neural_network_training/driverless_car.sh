7949cf49dc38a1b151f8a0cf6e468921eefaddcd
Kyle from PI <kylehigginson11@hotmail.com>
2018-01-27 15:40:54 +0000

    Slight mod to service script

--- neural_network_training/driverless_car.sh
+++ neural_network_training/driverless_car.sh
@@ -1,35 +1,16 @@
 #! /bin/bash
-# Copyright (c) 1996-2012 My Company.
-# All rights reserved.
-#
-# /etc/init.d/driverless_car
-#
-### BEGIN INIT INFO
-# Provides: testdaemon
-# Required-Start:
-# Should-Start:
-# Required-Stop:
-# Should-Stop:
-# Default-Start:  3 5
-# Default-Stop:   0 1 2 6
-# Short-Description: Test daemon process
-# Description:    Runs up the test daemon process
-### END INIT INFO
-
-# Activate the python virtual environment
 
 case "$1" in
   start)
     echo "Starting server"
     # Start the daemon
-    cd ~/repos/self_driving_car/neural_network_training/
+    cd /home/pi/repos/self_driving_car/neural_network_training/
     sudo python3 self_drive.py &
-    LASTPID=$!
     ;;
   stop)
     echo "Stopping server"
     # Stop the daemon
-    kill $LASTPID
+    sudo pkill python
     ;;
   restart)
     echo "Restarting server"
